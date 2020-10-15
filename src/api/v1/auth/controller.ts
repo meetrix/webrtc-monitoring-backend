4c9a42714c92b19eaed294bef69a204b5b8aae24
ManoRanjana De Silva <manoranjana@ManoRanjanas-MacBook-Pro.local>
2020-10-15 13:19:42 +0530

    Social auth verification status on db added

--- src/api/v1/auth/controller.ts
+++ src/api/v1/auth/controller.ts
@@ -1,15 +1,18 @@
 import { Response, NextFunction } from 'express';
 import { signToken } from '../../../util/auth';
-import { UserDocument } from '../../../models/User';
+import { UserDocument, User } from '../../../models/User';
 import { AUTH_LANDING } from '../../../config/settings';
 
-export const authCallback = (req: any, res: Response, next: NextFunction): void => {
+export const authCallback = async (req: any, res: Response, next: NextFunction): Promise<void> => {
     try {
         if (!req.user) {
             throw 'User not found';
         }
         const user = req.user as UserDocument;
         res.redirect(`${AUTH_LANDING}?token=${signToken(user)}`);
+        user.isVerified = true,
+            await user.save();
+
     } catch (error) {
         next(error);
     }
