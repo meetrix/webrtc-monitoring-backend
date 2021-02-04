9cf4f51d606d34310d79d0df6f34a90849c50f45
Wickramaranga Aneygunawardhana <wickramaranga@meetrix.io>
2021-02-05 04:17:04 +0530

    Add rate limiting for /feedback and /recording
    Add rate limiting using `rate-limiter-flexible` for the above
    unprotected routes. Limited to 20 requests per minute per IP addr.

--- src/api/v1/recording/index.ts
+++ src/api/v1/recording/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
-import { isAuthenticated } from '../../../middleware';
+import rateLimiterMiddleware from '../../../middleware/rateLimiterMemory';
 
 import { trackRecording } from './controller';
 
 const router = express.Router();
 
-router.post('/', trackRecording);
+router.post('/', rateLimiterMiddleware, trackRecording);
 
 export const recordingRouter = router;
---
+++ src/middleware/rateLimiterMemory.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { RateLimiterMemory } from 'rate-limiter-flexible';
+
+const rateLimiter = new RateLimiterMemory({
+  keyPrefix: 'middleware',
+  points: 20, // 20 requests
+  duration: 60, // per 1 minute by IP
+});
+
+const rateLimiterMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  rateLimiter.consume(req.ip)
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      res.status(429).json({ success: false, error: 'Too Many Requests' });
+    });
+};
+
+export default rateLimiterMiddleware;
