73ce467d1068b4d6a7c459702604e06ecfb453ce
Wickramaranga Aneygunawardhana <wickramaranga@meetrix.io>
2021-02-04 23:08:43 +0530

    Limit recording tracking to registered users
    - Rate limiting or some mechanism should be applied to non-protected
    routes

--- src/api/v1/recording/index.ts
+++ src/api/v1/recording/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import { isAuthenticated } from '../../../middleware';
+
 import { trackRecording } from './controller';
 
 const router = express.Router();
 
-router.post('/', trackRecording);
+router.post('/', isAuthenticated, trackRecording);
 
 export const recordingRouter = router;
