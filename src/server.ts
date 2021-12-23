7fda476d404317844e50695a10c77392a0b18755
Buddhika Jayawardhana <buddhika.anushka@gmail.com>
2021-12-23 11:13:33 +1000

    Install socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import app from './app';
 import logger from './util/logger';
 
-const server = app.listen(app.get('port'), (): void => {
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+httpServer.listen(app.get('port'), (): void => {
   logger.info(
     `App is running at http://localhost:${app.get('port')} in ${app.get(
       'env'
     )} mode`
   );
 });
-
-export default server;
+export default httpServer;
