4917470ea543a58e09cab7ec3d9a8e88bcd908c2
Wickramaranga Abeygunawardhana <wickramaranga@meetrix.io>
2022-06-10 12:56:29 +0000

    feat/WMS-48: ICE server config functionality

--- test/integration/plugin.test.ts
+++
@@ -1,8 +0,0 @@
-import { initMongo, disconnectMongo, clearDB } from '../setup';
-
-describe('Plugin', () => {
-  beforeAll(async () => await initMongo());
-  afterAll(async () => await disconnectMongo());
-
-  it('should create a plugin', () => {});
-});
